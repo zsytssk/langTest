d095df191d573fbebc6447155232e33c588c46af
zsytssk <zsytssk@gmail.com>
2021-04-02 10:05:17 +0800

    master:

--- src/main.rs
+++ src/main.rs
@@ -15,28 +15,17 @@ fn main() {
     let path = env::current_dir().expect("err");
     println!("{}", path.display());
     let unparsed_file = fs::read_to_string("src/number.csv").expect("cannot read file");
-    let file = CSVParser::parse(Rule::file, &unparsed_file)
+    let file = CSVParser::parse(Rule::enclosed, &unparsed_file)
         .expect("unsuccessful parse") // unwrap the parse result
         .next()
         .unwrap();
 
-    let mut field_sum: f64 = 0.0;
-    let mut record_count: u64 = 0;
-
-    for record in file.into_inner() {
-        match record.as_rule() {
-            Rule::record => {
-                record_count += 1;
-
-                for field in record.into_inner() {
-                    field_sum += field.as_str().parse::<f64>().unwrap();
-                }
+    for enclosed in file.into_inner() {
+        match enclosed.as_rule() {
+            Rule::number => {
+                println!("{}", enclosed.as_str())
             }
-            Rule::EOI => (),
             _ => unreachable!(),
         }
     }
-
-    println!("Sum of fields: {}", field_sum);
-    println!("Number of records: {}", record_count);
 }
