0651a493255e2a641ab8b86d1a762fcb55129c6e
zsytssk <zsytssk@gmail.com>
2021-04-02 09:57:18 +0800

    master:

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@ extern crate pest_derive;
 use pest::Parser;
 use std::fs;
 
+use std::env;
+
 #[derive(Parser)]
 #[grammar = "csv.pest"]
 pub struct CSVParser;
 
 fn main() {
-    let unparsed_file = fs::read_to_string("./number.csv").expect("cannot read file");
+    let path = env::current_dir().expect("err");
+    println!("{}", path.display());
+    let unparsed_file = fs::read_to_string("src/number.csv").expect("cannot read file");
     let file = CSVParser::parse(Rule::file, &unparsed_file)
         .expect("unsuccessful parse") // unwrap the parse result
         .next()
