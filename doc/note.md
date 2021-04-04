41678434d54641eaacb095a6ba0f5d8edaf96fe2
zsytssk <zsytssk@gmail.com>
2021-04-04 12:36:08 +0800

    master:

--- doc/note.md
+++ doc/note.md
@@ -1,6 +1,9 @@
-https://pest.rs/book/
-https://pest.rs/book/examples/csv.html
+https://pest.rs/book/examples/ini.html
+
+`let a = 1;`
 
 ## 2021-01-04 11:23:14
 
+- @todo 写一个简单的解析 const a = {...} 这种形式的解析器
+  - 怎么匹配字母 空格
 - @ques `#[macro_use]` 能不能放到 Cargo.toml 中
--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,20 @@ extern crate pest_derive;
 use pest::Parser;
 use std::fs;
 
-use std::env;
-
 #[derive(Parser)]
 #[grammar = "csv.pest"]
 pub struct CSVParser;
 
 fn main() {
-    let path = env::current_dir().expect("err");
-    println!("{}", path.display());
     let unparsed_file = fs::read_to_string("src/number.csv").expect("cannot read file");
-    let file = CSVParser::parse(Rule::enclosed, &unparsed_file)
-        .expect("unsuccessful parse") // unwrap the parse result
-        .next()
-        .unwrap();
+    let parse_result = CSVParser::parse(Rule::statement, &unparsed_file);
 
-    for enclosed in file.into_inner() {
-        match enclosed.as_rule() {
-            Rule::number => {
-                println!("{}", enclosed.as_str())
-            }
-            _ => unreachable!(),
+    match parse_result {
+        Ok(mut pairs) => {
+            let pair = pairs.next().unwrap();
+            println!("{:?}", pair.as_str());
+            println!("{:?}", pair);
         }
+        Err(_err) => {}
     }
 }
