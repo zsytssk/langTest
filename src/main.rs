a704b2f05cf54d3579f29895bcff874c4e09e671
zsytssk <zsytssk@gmail.com>
2021-04-06 10:51:08 +0800

    master:

--- src/main.rs
+++ src/main.rs
@@ -11,34 +11,10 @@ pub struct CSVParser;
 
 fn main() {
     let unparsed_file = fs::read_to_string("src/number.csv").expect("cannot read file");
-    let parse_result = CSVParser::parse(Rule::file, &unparsed_file)
+    let parse_result = CSVParser::parse(Rule::test, &unparsed_file)
         .unwrap()
         .next()
-        .unwrap()
-        .into_inner();
+        .unwrap();
 
-    let mut properties: HashMap<&str, HashMap<&str, &str>> = HashMap::new();
-    let mut current_section_name = "";
-    for item in parse_result {
-        match item.as_rule() {
-            Rule::section => {
-                let mut inner_rules = item.into_inner();
-                current_section_name = inner_rules.next().unwrap().as_str();
-            }
-            Rule::property => {
-                let mut inner_rules = item.into_inner();
-                let name = inner_rules.next().unwrap().as_str();
-                let value = inner_rules.next().unwrap().as_str();
-
-                let section = properties.entry(current_section_name).or_default();
-                section.insert(name, value);
-            }
-            Rule::EOI => (),
-            _ => {
-                println!("error {} xdxx", item.as_str())
-            }
-        }
-    }
-
-    println!("{:#?}", properties);
+    println!("{:#?}", parse_result);
 }
