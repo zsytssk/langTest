5eaa6fb6e1a8df22b488d862ab6075e1f2ef6b2f
zsytssk <zsytssk@gmail.com>
2021-04-09 10:01:27 +0800

    master:

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,30 @@
 #[macro_use]
 extern crate pest_derive;
 
-use pest::Parser;
-use std::{collections::HashMap, fs};
+ pest::Parser;
+use std::fs;
+
+use std::env;
 
 #[derive(Parser)]
 #[grammar = "csv.pest"]
 pub struct CSVParser;
 
 fn main() {
+    let path = env::current_dir().expect("err");
+    println!("{}", path.display());
     let unparsed_file = fs::read_to_string("src/number.csv").expect("cannot read file");
-    let parse_result = CSVParser::parse(Rule::test, &unparsed_file).unwrap();
+    let file = CSVParser::parse(Rule::enclosed, &unparsed_file)
+        .expect("unsuccessful parse") // unwrap the parse result
+        .next()
+        .unwrap();
 
-    println!("{}", parse_result.as_str());
+    for enclosed in file.into_inner() {
+        match enclosed.as_rule() {
+            Rule::number => {
+                println!("{}", enclosed.as_str())
+            }
+            _ => unreachable!(),
+        }
+    }
 }
