45e5dc0e535dfbb7af9e691c65f4a429a7813a23
zsytssk <zsytssk@gmail.com>
2021-04-10 14:06:34 +0800

    master:

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,13 @@ fn main() {
     let path = env::current_dir().expect("err");
     println!("{}", path.display());
     let unparsed_file = fs::read_to_string("src/number.csv").expect("cannot read file");
-    let file = CSVParser::parse(Rule::json, &unparsed_file)
+    let file = CSVParser::parse(Rule::object, &unparsed_file)
         .expect("unsuccessful parse") // unwrap the parse result
-        .next()
-        .unwrap();
+        ;
 
-    for item in file.into_inner() {
-        println!("{:?}", item.as_str());
-    }
+    println!("{:#?}", file);
+
+    // for item in file.into_inner() {
+    //     println!("{:?}", item.as_str());
+    // }
 }
